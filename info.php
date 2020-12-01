a5d88c895420cad42ff93b07c58cdf641f226ba9
chloegjones <chloegrcjones@gmail.com>
2020-12-01 14:26:13 -0600

    updated info.php to connect to practice database
    updated info.php to connect to practice database and inserted my first name and last name to make sure the php worked correctly and inserted into database. It was successful.

--- info.php
+++ info.php
@@ -1,1 +1,31 @@
+<!DOCTYPE html>
+<html>
+<body>
 
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "DiFuAcv5TaGEDJ";
+$dbname = "practice";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO Patient (first, last)
+VALUES ('$_POST[first]', '$_POST[last]')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
+
+</body>
+</html>
